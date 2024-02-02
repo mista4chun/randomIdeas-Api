64b8b5b18c1e91be41c432e2d1292820ddb1615e
Fortune Samson <mr4chun4u@gmail.com>
2024-02-02 07:44:15 +0100

    Final Commit FullStack RandomIdeas API

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -4,24 +4,50 @@ class IdeaList {
   constructor() {
     this._ideaListEl = document.querySelector('#idea-list');
     this._ideas = [];
-    this.getIdeas()
+    this.getIdeas();
     this._validTags = new Set();
     this._validTags.add('technology');
     this._validTags.add('software');
     this._validTags.add('business');
     this._validTags.add('eduction');
     this._validTags.add('health');
-    this._validTags.add('invention');
+    this._validTags.add('inventions');
   }
 
- async  getIdeas() {
+  addEventlistener() {
+    this._ideaListEl.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-times')) {
+        e.stopImmediatePropagation();
+        const ideaId = e.target.parentElement.parentElement.dataset.id;
+        this.deleteIdea(ideaId);
+      }
+    });
+  }
+
+  async getIdeas() {
     try {
-        const res = await IdeasApi.getIdeas()
-        this._ideas = res.data.data
-        console.log(this._ideas)
+      const res = await IdeasApi.getIdeas();
+      this._ideas = res.data.data;
+      this.render();
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
+  }
+
+  async deleteIdea(ideaId) {
+    try {
+      // Delete from server
+      const res = await IdeasApi.deleteIdea(ideaId);
+      this._ideas.filter((idea) => idea._id !== ideaId);
+      this.getIdeas();
+    } catch (error) {
+      alert('You can not delete this resource');
+    }
+  }
+
+  addIdeaToList(idea) {
+    this._ideas.push(idea);
+    this.render();
   }
 
   getTagClass(tag) {
@@ -39,9 +65,13 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {
         const tagCLass = this.getTagClass(idea.tag);
+        const deleteBtn =
+          idea.username === localStorage.getItem('username')
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`
+            : '';
         return `
-            <div class="card">
-            <button class="delete"><i class="fas fa-times"></i></button>
+            <div class="card" data-id="${idea._id}">
+            ${deleteBtn}
             <h3>
               ${idea.text}
             </h3>
@@ -53,6 +83,7 @@ class IdeaList {
           </div>`;
       })
       .join('');
+    this.addEventlistener();
   }
 }
 
--- routes/ideas.js
+++ routes/ideas.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Idea = require('../models/Idea');
 
-
-
 // Get All ideas
 router.get('/', async (req, res) => {
   try {
@@ -46,17 +44,27 @@ router.post('/', async (req, res) => {
 // Update idea
 router.put('/:id', async (req, res) => {
   try {
-    const updatedIdea = await Idea.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: {
-          text: req.body.text,
-          tag: req.body.tag,
+    const idea = await Idea.findById(req.params.id);
+    // Match the usernames
+    if (idea.username === req.body.username) {
+      const updatedIdea = await Idea.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            text: req.body.text,
+            tag: req.body.tag,
+          },
         },
-      },
-      { new: true }
-    );
-    res.json({ success: true, data: updatedIdea });
+        { new: true }
+      );
+      return res.json({ success: true, data: updatedIdea });
+    }
+    // Does not match username
+   
+    res.status(403).json({
+      success: false,
+      error: 'you are not authorized to update this resource',
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, error: 'Something went wrong' });
@@ -66,8 +74,20 @@ router.put('/:id', async (req, res) => {
 // Delete idea
 router.delete('/:id', async (req, res) => {
   try {
-    await Idea.findByIdAndDelete(req.params.id);
-    res.json({ success: true, data: {} });
+    const idea = await Idea.findById(req.params.id);
+
+    // Match the usernames
+    if (idea.username === req.body.username) {
+      await Idea.findByIdAndDelete(req.params.id);
+      return res.json({ success: true, data: {} });
+    }
+
+    // Does not match username
+   
+    res.status(403).json({
+      success: false,
+      error: 'You are not authorized to delete this resource',
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, error: 'Something went wrong' });
